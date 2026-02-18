1e7bfcbdfd216e98647391f360b1eda066d7c90a
nicolasorteg <nicolasortegaf22@gmail.com>
2026-02-18 17:46:35 +0100

    quick fix

--- Gestión Empresa Héroes/Gestion/Config/HeroesConfig.cs
+++ Gestión Empresa Héroes/Gestion/Config/HeroesConfig.cs
@@ -5,5 +5,6 @@ public static class HeroesConfig {
     public const int EnergiaMaxima = 200;
     public const int MultiplicadorNivelCalcPoder = 100;
     public const int FuerzaMaxima = 50;
+    public const int AgilidadMaxima = 50;
     public static int[] UmbralesNivel = [10, 25, 50, 100, 150, 225, 325, 450, 600, 800, 0];
 }
--- Gestión Empresa Héroes/Gestion/Models/HeroeInteligente.cs
+++ Gestión Empresa Héroes/Gestion/Models/HeroeInteligente.cs
@@ -3,10 +3,10 @@
 namespace Gestion.Models;
 
 public sealed record HeroeInteligente: Heroe {
-    public required Agilidades Agilidad { get; set; }
-    public enum Agilidades { Baja = 10, Media = 30, Alta = 50 }
+    public required Inteligencias Inteligencia { get; set; }
+    public enum Inteligencias { Baja = 10, Media = 30, Alta = 50 }
     
-    public override int CalcularPoder() => Energia + Nivel * HeroesConfig.MultiplicadorNivelCalcPoder + (int)Agilidad + (int)Rareza;
+    public override int CalcularPoder() => Energia + Nivel * HeroesConfig.MultiplicadorNivelCalcPoder + (int)Inteligencia + (int)Rareza;
     
-    public override string ToString() => base.ToString() + $"  |  Agilidad: {Agilidad}";
+    public override string ToString() => base.ToString() + $"  |  Inteligencia: {Inteligencia}";
 }
