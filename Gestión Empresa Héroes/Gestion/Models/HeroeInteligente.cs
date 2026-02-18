4072b71fce815dd7d2c4b4bb64f2293d46cca998
nicolasorteg <nicolasortegaf22@gmail.com>
2026-02-18 17:30:31 +0100

    models hechos

---
+++ Gestión Empresa Héroes/Gestion/Models/HeroeInteligente.cs
@@ -0,0 +1,12 @@
+﻿using Gestion.Config;
+
+namespace Gestion.Models;
+
+public record HeroeInteligente: Heroe {
+    public required Agilidades Agilidad { get; set; }
+    public enum Agilidades { Baja = 10, Media = 30, Alta = 50 }
+    
+    public override int CalcularPoder() => Energia + Nivel * HeroesConfig.MultiplicadorNivelCalcPoder + (int)Agilidad + (int)Rareza;
+    
+    public override string ToString() => base.ToString() + $"  |  Agilidad: {Agilidad}";
+}
