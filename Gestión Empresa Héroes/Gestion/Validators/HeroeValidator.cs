ae13b404d8005512bdddf9f866158eb51caf898a
nicolasorteg <nicolasortegaf22@gmail.com>
2026-02-20 18:38:17 +0100

    start validators

---
+++ Gestión Empresa Héroes/Gestion/Validators/HeroeValidator.cs
@@ -0,0 +1,9 @@
+﻿using Gestion.Models;
+
+namespace Gestion.Validators;
+
+public class HeroeValidator: IValidator<Heroe> {
+    public IEnumerable<string> Validar(Heroe h) {
+        throw new NotImplementedException();
+    }
+}
---
+++ Gestión Empresa Héroes/Gestion/Validators/IValidator.cs
@@ -0,0 +1,5 @@
+﻿namespace Gestion.Validators;
+
+public interface IValidator<in T> {
+    IEnumerable<string> Validar(T entity);
+}
