93c0c646d3fa6033ed619781280e952163fc87a5
nicolasorteg <nicolasortegaf22@gmail.com>
2026-02-21 21:39:19 +0100

    validator heroe

--- Gestión Empresa Héroes/Gestion/Config/HeroesConfig.cs
+++ Gestión Empresa Héroes/Gestion/Config/HeroesConfig.cs
@@ -5,6 +5,7 @@ public static class HeroesConfig {
     public const int EnergiaMinima = 10;
     public const int EnergiaMaxima = 200;
     public const int MultiplicadorNivelCalcPoder = 100;
+    public const int NivelMaximo = 10;
     public const int FuerzaMaxima = 50;
     public const int AgilidadMaxima = 50;
     public static int[] UmbralesNivel = [10, 25, 50, 100, 150, 225, 325, 450, 600, 800, 0];
--- Gestión Empresa Héroes/Gestion/Validators/HeroeValidator.cs
+++ Gestión Empresa Héroes/Gestion/Validators/HeroeValidator.cs
@@ -1,9 +1,55 @@
-﻿using Gestion.Models;
+﻿using Gestion.Config;
+using Gestion.Models;
 
 namespace Gestion.Validators;
 
+/// <summary>
+/// Validador especializado para la entidad Heroe.
+/// Acumula los errores encontrados.
+/// </summary>
 public class HeroeValidator: IValidator<Heroe> {
     public IEnumerable<string> Validar(Heroe h) {
-        throw new NotImplementedException();
+        var errores = new List<string>();
+
+        // si no es un héroe
+        if (h is not { } heroe) {
+            errores.Add("La entidad proporcionada no es un Héroe.");
+            return errores;
+        }
+        
+        // si el nombre es nulo, es un espacio en blanco o tiene una longitud menor o igual a 1 caracter
+        if (string.IsNullOrWhiteSpace(h.Nombre) || h.Nombre.Length < 2) 
+            errores.Add("El nombre es obligatorio (mín. 2 car.).");
+
+        // si el nivel es negativo o mayor a 10
+        if (int.IsNegative(h.Nivel) || h.Nivel > HeroesConfig.NivelMaximo )
+            errores.Add($"El nivel es obligatorio y no puede ser mayor de {HeroesConfig.NivelMaximo}.");
+        
+        // si la energia es negativa o mayor a 200
+        if (int.IsNegative(h.Energia) || h.Energia > HeroesConfig.EnergiaMaxima )
+            errores.Add($"La energía es obligatoria y no puede ser mayor de {HeroesConfig.EnergiaMaxima}.");
+        
+
+        // si la energia es negativa o mayor a 200
+        if (int.IsNegative(h.Energia) || h.Energia > HeroesConfig.EnergiaMaxima )
+            errores.Add($"La energía es obligatoria y no puede ser mayor de {HeroesConfig.EnergiaMaxima}.");
+        
+        // si la experiencia es negativa o no corresponde al umbral de subida correspondiente al nivel
+        if (int.IsNegative(h.Experiencia)) {
+            errores.Add("La experiencia no puede ser un valor negativo.");
+        }
+        else if (h.Nivel < HeroesConfig.UmbralesNivel.Length - 1) {
+            // si la experiencia es mayor o igual al umbral
+            if (h.Experiencia >= HeroesConfig.UmbralesNivel[h.Nivel]) {
+                errores.Add($"La experiencia para el nivel {h.Nivel} debe ser menor a {HeroesConfig.UmbralesNivel[h.Nivel]} (umbral de subida).");
+            }
+        }
+
+        // si la rareza no corresponde con una existente
+        if (!Enum.IsDefined(h.Rareza)) {
+            errores.Add("La rareza debe ser una de las 5 posibles. (Común, Especial, Raro, Épico y Legendario)");
+        }
+        
+        return errores;
     }
 }
