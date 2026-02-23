29f22206b4bd3111fb3c2154f0619b1a1df7f174
nicolasorteg <nicolasortegaf22@gmail.com>
2026-02-23 04:39:44 +0100

    1a version heroerepo

--- Gestión Empresa Héroes/Gestion/Repositories/HeroeRepository.cs
+++ Gestión Empresa Héroes/Gestion/Repositories/HeroeRepository.cs
@@ -3,24 +3,66 @@
 namespace Gestion.Repositories;
 
 public class HeroeRepository: IHeroeRepository {
-    
-    
-    
-    
-    
-    public IEnumerable<Heroe> GetAll() {
-        throw new NotImplementedException();
+
+    // singleton para instancia unica
+    private static HeroeRepository? _instance;
+    public static HeroeRepository GetInstance() {
+        return _instance ??= new HeroeRepository();
     }
-    public Heroe? GetById(int id) {
-        throw new NotImplementedException();
+    
+    // listado de heroes
+    private readonly List<Heroe> _heroes = [];
+
+    
+    private static int _idCounter;
+    
+    private HeroeRepository() {
+        InitMiembros();
     }
+
+    private void InitMiembros() {
+        var miembrosIniciales = HeroeFactory.DemoHeroes();
+        foreach (var h in miembrosIniciales) Create(h);
+    }
+    
+    private static int GetNextId() => ++_idCounter;
+
+
+    /// <inheritdoc cref="IHeroeRepository.GetAll" />
+    public IEnumerable<Heroe> GetAll() => _heroes;
+    
+    /// <inheritdoc cref="IHeroeRepository.GetById" />
+    public Heroe? GetById(int id) => _heroes.FirstOrDefault(h => h.Id == id);
+    
+    /// <inheritdoc cref="IHeroeRepository.Create" />
     public Heroe? Create(Heroe entity) {
-        throw new NotImplementedException();
+        if (GetById(entity.Id) != null) return null;
+        
+        var nuevoConId = entity with { Id = GetNextId() };
+        _heroes.Add(nuevoConId);
+        return entity;
     }
+    
+    /// <inheritdoc cref="IHeroeRepository.Update" />
     public Heroe? Update(int id, Heroe entity) {
-        throw new NotImplementedException();
+        var existente = GetById(id);
+        if (existente == null) return null;
+        
+        existente.Nombre = entity.Nombre;
+        existente.Nivel = entity.Nivel;
+        existente.Energia = entity.Energia;
+        existente.Experiencia = entity.Experiencia;
+        existente.Rareza = entity.Rareza;
+        
+        return existente;
     }
+    
+    /// <inheritdoc cref="IHeroeRepository.Delete" />
     public Heroe? Delete(int id) {
-        throw new NotImplementedException();
+        var heroe = GetById(id);
+        if (heroe != null) {
+            _heroes.Remove(heroe);
+        }
+        return heroe;
     }
 }
